eba408308a7994c2e7c7da8bcb0c36da85477242
Maciej Rutkowski <303179@stud.umk.pl>
2022-05-14 18:53:17 +0200

    Implementacja symulowanego wyżarzania

--- SimulatedAnnealing/SimulatedAnnealing/Program.cs
+++ SimulatedAnnealing/SimulatedAnnealing/Program.cs
@@ -1,77 +1,11 @@
-﻿using SimulatedAnnealing.Data;
-using SimulatedAnnealing.Scheduler;
-using SimulatedAnnealing.SimulatedAnnealing.Data.LazyStore;
-
-namespace SimulatedAnnealing
+﻿namespace SimulatedAnnealing
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // 1<3<5 i 2<4
-            List<Requirement> requirements = new List<Requirement>()
-            {
-                new Requirement(){ JobId = 3, RequiresId = 1 },
-                new Requirement(){ JobId = 5, RequiresId = 3 },
-                new Requirement(){ JobId = 5, RequiresId = 1 },
-                new Requirement(){ JobId = 4, RequiresId = 2 },
-            };
-
-            ISequenceStore sequenceStore = new LazySequenceStore(requirements);
-            List<JobSequence> jobs = new List<JobSequence>();
-            for (int i = 0; i < 1300; i++)
-            {
-                jobs.Add(sequenceStore.GetSequence(i));
-            }
-            JobScheduler scheduler = new(requirements);
-            int min = 100;
-            JobSequence best= null;
-            foreach (JobSequence job in jobs)
-            {
-                /*
-                Console.Write("\nP1: ");
-                foreach(Job j in job.P1Jobs)
-                {
-                    Console.Write($"{j.Id} ");
-                }
-
-                Console.Write("\nP2: ");
-                foreach (Job j in job.P2Jobs)
-                {
-                    Console.Write($"{j.Id} ");
-                }
-                */
-                var cost = scheduler.RunJobs(job);
-                if(cost < min)
-                {
-                    min = cost;
-                    best = job;
-                }
-                
-                Console.WriteLine($"\nCost: {cost}");
-            }
-
-            Console.WriteLine($"Min: {min}");
-            JobScheduler schedulerDraw = new(requirements, true);
-            schedulerDraw.RunJobs(best);
-
-            /*
-            List<Job> p1Jobs = new List<Job>()
-            {
-                new Job(){ Id = 1, Cost = 1},
-                new Job(){ Id = 3, Cost = 2},
-                new Job(){ Id = 2, Cost = 3},
-            };
-            List<Job> p2Jobs = new List<Job>()
-            {
-                //new Job(){ Id = 3, Cost = 2}
-                new Job(){ Id = 4, Cost = 8},
-            };
-            JobSequence sequence = new JobSequence(p1Jobs, p2Jobs);
-
-            JobScheduler scheduler = new(requirements);
-            Console.WriteLine($"Cost: {scheduler.RunJobs(sequence)}");
-            */
+            SA.SimulatedAnnealing sa = new SA.SimulatedAnnealing();
+            sa.Run();
         }
     }
 }
---
+++ SimulatedAnnealing/SimulatedAnnealing/SimulatedAnnealing.cs
@@ -0,0 +1,83 @@
+﻿using SimulatedAnnealing.Data;
+using SimulatedAnnealing.Scheduler;
+using SimulatedAnnealing.SimulatedAnnealing.Data.LazyStore;
+
+namespace SA
+{
+    public class SimulatedAnnealing
+    {
+        private readonly JobScheduler _jobScheduler;
+        private readonly ISequenceStore _sequenceStore;
+        private readonly List<Requirement> _requirements;
+        private readonly Random _random;
+
+        public SimulatedAnnealing()
+        {
+            // 1<3<5 i 2<4
+            List<Requirement> requirements = new List<Requirement>()
+            {
+                new Requirement(){ JobId = 3, RequiresId = 1 },
+                new Requirement(){ JobId = 5, RequiresId = 3 },
+                new Requirement(){ JobId = 5, RequiresId = 1 },
+                new Requirement(){ JobId = 4, RequiresId = 2 },
+            };
+
+            _requirements = requirements;
+            _jobScheduler = new JobScheduler(requirements);
+            _sequenceStore = new LazySequenceStore(requirements);
+            _random = new Random();
+        }
+
+        public void Run()
+        {
+            int minX = 0;
+            int globalMinX = 0;
+
+            int currentX = 1;
+            int newX;
+
+            double T = 1000;
+            double alpha = 0.3;
+
+            while (T > 0)
+            {
+                if (GetValue(currentX) < GetValue(minX))
+                {
+                    minX = currentX;
+                    if (GetValue(minX) < GetValue(globalMinX))
+                    {
+                        globalMinX = minX;
+                    }
+                }
+
+                newX = GetNeighbour();
+                if (Math.Exp((GetValue(currentX) - GetValue(newX)) / T) > RandomDouble(0, 1))
+                {
+                    currentX = newX;
+                }
+
+                T *= alpha;
+            }
+
+            Console.WriteLine($"MIN: {GetValue(globalMinX)}\n");
+            JobScheduler schedulerDraw = new JobScheduler(_requirements, true);
+            schedulerDraw.RunJobs(_sequenceStore.GetSequence(globalMinX));
+        }
+
+        private int GetNeighbour()
+        {
+            return _random.Next(9000);
+        }
+
+        private int GetValue(int x)
+        {
+            return _jobScheduler.RunJobs(_sequenceStore.GetSequence(x));
+        }
+
+        private double RandomDouble(double minValue, double maxValue)
+        {
+            var next = _random.NextDouble();
+            return minValue + (next * (maxValue - minValue));
+        }
+    }
+}
